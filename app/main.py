3c4ca35d7271f54f3c6084d0ba45e79fc6d7fa8f
Dmytro <@email.com>
2026-07-30 02:54:04 +0300

    2nd

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ class CarWashStation:
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         new_average = ((self.average_rating * self.count_of_ratings + rate)
